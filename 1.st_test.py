bc33c6fbd70452b911cc4c85263ff311fcc87e64
jeey <wldudsmile97@gmail.com>
2025-03-12 12:04:14 +0900

    streamlit_test_code

---
+++ 1.st_test.py
@@ -0,0 +1,8 @@
+# streamlit 실행 방법 : 
+## streamlit run 1.st_test.py
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ai_streamlit_ex
 Streamlit 실습
+
+# 설치 라이브러리 
+pip install openai
+pip install streamlit
